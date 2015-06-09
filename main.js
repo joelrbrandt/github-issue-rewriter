1e1e84f1908e9c1a2ce62aa47365ec2be3ce4d67
Joel Brandt <joelrbrandt@gmail.com>
2015-06-09 16:58:13 -0700

    moar

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 (function () {
     "use strict";
 
+    var ISSUES_PER_PAGE = 100;
+
     var config = require("./config"),
-        Github = require("github");
+        Github = require("github"),
+        Promise = require("bluebird");
 
     var api = new Github({
         version: "3.0.0",
@@ -21,14 +24,38 @@
     });
 
 
-    api.issues.repoIssues({
-        user: config.repoUser,
-        repo: config.repoName
-    }, function (err, result) {
-        console.log(JSON.stringify({
-            err: err,
-            result: result
-        }, null, "  "));
+    var repoIssues = Promise.promisify(api.issues.repoIssues, api.issues);
+
+    var getIssuePage = function (n) {
+        return repoIssues({
+            user: config.repoUser,
+            repo: config.repoName,
+            state: "closed",
+            direction: "asc",
+            "per_page": ISSUES_PER_PAGE,
+            page: n
+        });
+    };
+
+    var getAllIssues = function () {
+        var all = [];
+
+        var getAllIssuesHelper = function (page) {
+            return getIssuePage(page).then(function (result) {
+                all = all.concat(result);
+                if (result.length > 0) {
+                    return getAllIssuesHelper(page + 1);
+                } else {
+                    return all;
+                }
+            });
+        };
+
+        return getAllIssuesHelper(1);
+    };
+
+    getAllIssues().then(function (result) {
+        console.log("got %d issues", result.length);
     });
 
 }());
